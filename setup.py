db72c63412a70a1fe0fd6188faba1decac3a354c
Ben Lacey <85163278+benlacey57@users.noreply.github.com>
2025-04-10 07:57:37 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+       name="python_library",
+       version="1.0",
+       packages=find_packages(),
+       description="A collection of Python scripts",
+       author="Ben Lacey",
+       author_email="ben.lacey57@gmail.com",
+   )
